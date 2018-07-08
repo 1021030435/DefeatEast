5b8acb94eba60dc531e5c08218a30fb5229bb886
Lenovo <Lenovo@G450>
2018-07-08 17:08:32 +0800

    redis

--- DefeatEast-dbc-Redis/src/main/java/com/app/redis/config/RedisConfig.java
+++ DefeatEast-dbc-Redis/src/main/java/com/app/redis/config/RedisConfig.java
@@ -1,69 +1,30 @@
-//package com.app.redis.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
+package com.app.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 //import org.springframework.core.io.ClassPathResource;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 //import org.springframework.data.redis.core.script.DefaultRedisScript;
 //import org.springframework.scripting.support.ResourceScriptSource;
-//
-//import redis.clients.jedis.JedisPoolConfig;
-//
-//
-////@Configuration
-//public class RedisConfig {
-//	
-////	@Bean
-////	public JedisPoolConfig pool() {
-////		JedisPoolConfig pool = new JedisPoolConfig();
-////		pool.setMaxWaitMillis(-1);
-////		pool.setMaxIdle(50000);// 最大空闲
-////		pool.setMinIdle(0);
-////		pool.setMaxTotal(50000);// 最大连接
-////		return pool;
-////	}
-////	@Bean
-////	public RedisConnectionFactory connection() {
-////		JedisConnectionFactory factory = new JedisConnectionFactory();
-////		factory.setHostName("127.0.0.1");
-////		factory.setPort(6379);
-////		factory.setTimeout(-1);
-////		return factory;
-////	}
-//
-//	@Bean
-//	  public RedisConnectionFactory redisConnectionFactory() {
-//
-//	    RedisStandaloneConfiguration config = new RedisStandaloneConfiguration("127.0.0.1", 6379);
-//	    JedisConnectionFactory jedis=	    new JedisConnectionFactory(config);
-//	    JedisPoolConfig pool = new JedisPoolConfig();
-//		pool.setMaxWaitMillis(-1);
-//		pool.setMaxIdle(50000);// 最大空闲
-//		pool.setMinIdle(0);
-//		pool.setMaxTotal(50000);// 最大连接
-//		jedis.setPoolConfig(pool);		
-//	    return  jedis;
-//	  }
-//	
-//	
-//
-//	@Bean
-//	public StringRedisTemplate stringTemplate(RedisConnectionFactory factory) {
-//		StringRedisTemplate template = new StringRedisTemplate(factory);
-//		return template;
-//	}
-//
-//	@Bean
-//	public RedisTemplate<?, ?> template(RedisConnectionFactory factory) {
-//		RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
-//		template.setConnectionFactory(factory);
-//		return template;
-//	}
-//
+
+@Configuration
+public class RedisConfig {
+
+	@Bean
+	public StringRedisTemplate stringTemplate(RedisConnectionFactory factory) {
+		StringRedisTemplate template = new StringRedisTemplate(factory);
+		return template;
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> template(RedisConnectionFactory factory) {
+		RedisTemplate<Object, Object> template = new RedisTemplate<Object, Object>();
+		template.setConnectionFactory(factory);
+		return template;
+	}
+
 //	@Bean
 //	public DefaultRedisScript<Boolean> redisScript() {
 //		DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<Boolean>();
@@ -71,4 +32,9 @@
 //		redisScript.setResultType(Boolean.class);
 //		return redisScript;
 //	}
-//}
+}
+
+/**
+ * RedisTemplate使用的是JdkSerializationRedisSerializer,redis中字节数组显示
+ * StringRedisTemplate使用的是StringRedisSerializer,redis中是正常的可读的字符串
+ */
